0202c1fbbc8065a5fee6e545c3e1c5abeb2583e7
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-08-23 20:46:45 +0300

    task2

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,18 @@
 import { Text } from 'components';
+import { useState } from 'react';
+import { Form } from '../components/Form/Form';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState([]);
+
+  function addToDo(value) {
+    setTodos(prevToDos => [...prevToDos, value]);
+  }
+  console.log(todos);
+  return (
+    <div>
+      <Form onSubmit={addToDo} />
+      <Text textAlign="center">There are no any todos ...</Text>
+    </div>
+  );
 };
